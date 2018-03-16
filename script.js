6aa907bcebedc76a71663d668232d8a8e6297bf5
Gab De Jesus <quillot2112@gmail.com>
2018-03-16 12:07:28 +0900

    Enclosed in IIFE, removed playCount for color gen

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let playCount = 1; //this is for checking how many times a player has played; playing at the start sets it to 1
+(function() {
 let numSquares = 6;
 let colors = [];
 // Get dom elements
@@ -60,7 +60,6 @@ function initOptions() {
     let buttons = document.getElementsByClassName('option');
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function() {
-            ++playCount;
             if(this.getAttribute('id') === "hardBtn") {
                 hardBtn.classList.add('selected');
                 easyBtn.classList.remove('selected');
@@ -76,55 +75,6 @@ function initOptions() {
         });
     }
 }
-
-
-// easyBtn.addEventListener("click", function(){
-//     ++playCount; //increment every time player plays again
-//     hardBtn.classList.remove("selected");
-//     easyBtn.classList.add("selected");
-//     numSquares = 3;
-//     colors = generateRandomColors(numSquares);
-//     pickedColor = pickRandomColor();
-//     colorDisplay.textContent = pickedColor;
-//     for(let i = 0; i < squares.length; i++ ) {
-//         if(colors[i]) {
-//             squares[i].style.background = colors[i];
-//         } else {
-//             squares[i].style.display = "none";
-//         }
-//     }
-// });
-
-// hardBtn.addEventListener("click", function(){
-//     ++playCount;  
-//     easyBtn.classList.remove("selected");
-//     hardBtn.classList.add("selected");
-//     numSquares = 6;
-//     colors = generateRandomColors(numSquares);
-//     pickedColor = pickRandomColor();
-//     colorDisplay.textContent = pickedColor;
-//     for(let i = 0; i < squares.length; i++ ) {  
-//             squares[i].style.background = colors[i];
-//             squares[i].style.display = "block";      
-//     }
-// });
-
-// resetButton.addEventListener("click",function(){
-//     ++playCount; //increment every time player plays again
-//     //generate reset colors
-//     colors = generateRandomColors(numSquares);
-//     //pick a new random color
-//     pickedColor = pickRandomColor();
-//     //change color display to match picked color
-//     colorDisplay.textContent = pickedColor;
-//     //change colors of the squares
-//     for(let i = 0; i < squares.length; i++ ) {
-//         squares[i].style.background = colors[i];
-//     }
-//     h1.style.background = "steelblue";
-//     messageDisplay.textContent = " ";
-//     this.textContent = "reset colors";
-// });
 
 // Setup squares colors
 function initSquares(squares) {
@@ -160,17 +110,8 @@ function pickRandomColor() {
 
 function generateRandomColors(num) {
     let colors = [];
-    if(playCount === 1) {
-        for(let i = 0; i < num; i++) {
-            colors.push(randomizeNewRGB([0, 0, 0]));
-        }
-    }
-    else {
-        let getCurrentColorDisplay = document.getElementById("colorDisplay");
-        let oldRGBValues = getCurrentColorDisplay.textContent.match(/[+-]?\d+(?:\.\d+)?/g);
-        for(let i = 0; i < num; i++) {
-            colors.push(randomizeNewRGB([0, 0, 0]));
-        }
+    for(let i = 0; i < num; i++) {
+        colors.push(randomizeNewRGB([0, 0, 0]));
     }
     return colors;
 }
@@ -180,3 +121,4 @@ function randomizeNewRGB(oldVals) {
     let [r, g, b] = oldVals.map(oldColorVal => (Math.floor(Math.random() * (256 - oldColorVal)) + oldColorVal) % 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
+})();
